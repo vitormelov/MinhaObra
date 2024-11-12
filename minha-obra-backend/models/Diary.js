718428c04e0f99c161a311642874499dfc126695
Vitormelov <vitormelov@gmail.com>
2024-11-12 09:37:06 -0300

    startTime e endTime no Diary

--- minha-obra-backend/models/Diary.js
+++ minha-obra-backend/models/Diary.js
@@ -1,9 +1,10 @@
-// src/models/Diary.js
 const mongoose = require('mongoose');
 
 const diarySchema = new mongoose.Schema({
   workId: { type: mongoose.Schema.Types.ObjectId, ref: 'Work', required: true },
   date: { type: Date, required: true },
+  startTime: { type: String, required: true }, // Campo de horário de início
+  endTime: { type: String, required: true },   // Campo de horário de término
   employees: [{ quantity: Number, role: String, company: String }],
   morningWeather: String,
   afternoonWeather: String,
